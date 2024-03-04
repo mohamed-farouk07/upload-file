fd80f804354e64af15db28ed513e4af8f4e63732
mohamed-farouk07 <60257984+mohamed-farouk07@users.noreply.github.com>
2024-03-04 11:51:30 +0200

    1

---
+++ upload-file/src/App.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+import './App.css';
+
+function App() {
+  
+	const [fileContent, setFileContent] = useState('');
+  const [totalWords, setTotalWords] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [key, setKey] = useState(0); // State to force re-render
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+
+    // Check if the file type is not text/plain (not a .txt file)
+    if (file && file.type !== 'text/plain') {
+      alert('Please select a .txt file.');
+      return;
+    }
+
+    setIsLoading(true); // Show loader
+
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const content = e.target.result;
+      setFileContent(content);
+
+      const words = content.split(/\s+/).filter(word => word !== ''); // Split by whitespace and remove empty strings
+      setTotalWords(words.length); // Calculate total number of words
+      setIsLoading(false); // Hide loader after processing
+    };
+
+    reader.readAsText(file);
+  };
+
+  const handleRefresh = () => {
+    setFileContent('');
+    setTotalWords(0);
+    setKey(prevKey => prevKey + 1); // Change the key to force re-render
+  };
+
+  return (
+    <div className="container">
+      <input key={key} type="file" onChange={handleFileChange} />
+      {isLoading && <div>Loading...</div>} {/* Display loader if isLoading is true */}
+      <div>
+        <h2>File Content:</h2>
+        <p className="file-content">{fileContent}</p>
+      </div>
+      <div>
+        <h2>Total Words:</h2>
+        <p className="total-words">{totalWords}</p>
+      </div>
+      <button className="refresh-button" onClick={handleRefresh}>Refresh</button>
+    </div>
+  );
+}
+
+export default App;
